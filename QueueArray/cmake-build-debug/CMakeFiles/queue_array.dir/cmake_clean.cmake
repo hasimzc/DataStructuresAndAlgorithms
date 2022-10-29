daac422c2d5b253f6ebf8415d0c38eb5d18cbe14
hasimzc <89254814+hasimzc@users.noreply.github.com>
2022-10-29 15:23:04 +0300

    Add files via upload

---
+++ QueueArray/cmake-build-debug/CMakeFiles/queue_array.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/queue_array.dir/main.cpp.obj"
+  "queue_array.exe"
+  "queue_array.exe.manifest"
+  "queue_array.lib"
+  "queue_array.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/queue_array.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
