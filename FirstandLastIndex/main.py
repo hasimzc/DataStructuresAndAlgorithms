f57ddbffa68747ebcc665e4c1111a55f7e685b10
hasimzc <89254814+hasimzc@users.noreply.github.com>
2022-11-06 13:45:13 +0300

    Update main.py

--- FirstandLastIndex/main.py
+++ FirstandLastIndex/main.py
@@ -1,3 +1,8 @@
+'''
+                     Problem Definition
+Given a sorted array of integers arr and an integer target, find the index of the first and last position of target in arr. In target can't be found in arr, return[-1,-1]
+'''
+
 liste = list(map(int,input().split()))
 def binarySearchForFirstIndex(target,liste,index):
     n = len(liste)
