409ceb0e84e4737bf4ea5c7b08f16e0d05699845
hasimzc <89254814+hasimzc@users.noreply.github.com>
2022-11-06 13:39:53 +0300

    Add files via upload

---
+++ KthLargestElement/main.py
@@ -0,0 +1,8 @@
+import heapq
+def kth_largest(arr,k):
+    array = [-i for i in arr]
+    heapq.heapify(array)
+    for i in range(k-1):
+        heapq.heappop(array)
+    return - heapq.heappop(array)
+print(kth_largest([3,5,2,4,1,6,4],2))
